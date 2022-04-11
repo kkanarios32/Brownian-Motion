b30100739ef887fc1e56678d7d53540e3cca5191
Kellen Kanarios <kellenkk@umich.edu>
2022-04-11 10:11:05 -0400

    final

--- code/training-data-generator.py
+++ code/training-data-generator.py
@@ -1,4 +1,3 @@
-from scipy.stats import norm  # For generating random numbers
 import matplotlib.pyplot as plt  # For ploting purpose
 import numpy as np
 import yfinance as yf
@@ -18,26 +17,52 @@ def geoBrownianMotion(sigma, mu, x, N, T):
         S_all.append(S)
     return S_all
 
-def brownianMotion(sigma, mu, N, T):
+def brownianMotion(sigma, mu, x, N, T):
     dt = float(T) / N
     dWt = np.random.normal(loc=0, scale=np.sqrt(dt))
     S_all = []
 
     for i in range(1, N):
         dWt = np.random.normal(loc=0, scale=np.sqrt(dt))
-        S = mu * dt + sigma * dWt
+        S = x + mu * dt + sigma * dWt
         S_all.append(S)
     return S_all
 
-def fitModel(data):
-   return 
+def fitModel(data, model):
+    if model:
+        data = np.log(data)
+    time = np.linspace(0,len(data), num=len(data))
+    A = np.vstack([time, np.ones(len(time))]).T
+    alpha = np.linalg.lstsq(A, data, rcond=None)
+    mu = alpha[0][0]
+    diff = []
+    for i in range(0, len(time)):
+        diff.append(data[i] - (mu * i))
+    diff = np.array(diff)
+    sigma = np.std(diff, ddof = 1)
+    if model:
+        model = geoBrownianMotion(sigma, mu, data[0], len(data), len(data))
+    else:
+        model = brownianMotion(sigma, mu, data[0], len(data), len(data))
+    return model
 
-def predictPrice(stock):
+
+
+def predictPrice(stock, model):
     info = yf.Ticker(stock)
-    data = info.history(period="10y")
-    prices = data['Close']
-    fitModel(prices)
+    data = info.history(period="1y")
+    prices = np.array(data['Close'])
+    get_error(prices)
 
-
-# plt.plot(brownianMotion(.5, 2, 1000, 5))
-# plt.show()
+def get_error(prices):
+    N = 1000
+    error = 0
+    for i in range(0,N):
+        fit = fitModel(prices, 1)
+        curr_err = np.mean(np.subtract(prices,fit))
+        plt.plot(fit)
+        error = error + (curr_err/N)
+    print(error)
+# predictPrice("fb",1)
+get_error(geoBrownianMotion(.5,.05,10000,252,1))
+plt.show()
